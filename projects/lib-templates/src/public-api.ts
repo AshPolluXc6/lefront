b5bcd1bfcabb5e3438fed7ef385175f1c5e961d7
oliEstevam <oliver.matheus@outlook.pt>
2024-11-06 00:07:30 -0300

    lib de componentes criada e configurada pronta para teste

--- projects/lib-templates/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of lib-templates
- */
-
-export * from './lib/lib-templates.service';
-export * from './lib/lib-templates.component';
-export * from './lib/components/index';
