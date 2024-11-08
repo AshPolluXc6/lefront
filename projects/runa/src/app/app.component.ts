111a8576e31e3284d0a82f719a5b504327a98836
oliEstevam <oliver.matheus@outlook.pt>
2024-11-08 07:42:23 -0300

    alterações na lib

---
+++ projects/runa/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CardCriticsComponent } from "../../../clib/src/lib/card-critics/card-critics.component";
+import { RateStarsComponent } from "../../../clib/src/lib/rate-stars/rate-stars.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CardCriticsComponent, RateStarsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Runa';
+}
