24276db1557e0bad598d4af680b047d7b462489d
Matheus Estevam <oliver.matheus@outlook.pt>
2024-11-21 18:14:03 -0300

    front atualizado

--- src/app/client/client-home/client-home.component.ts
+++ src/app/client/client-home/client-home.component.ts
@@ -5,35 +5,41 @@ import { FrameComponent } from "../../../../projects/clib/src/lib/frame/frame.co
 import { ConnectionService } from '../../services/connection.service';
 import { AnswerStandard } from '../../services/answerstandard.service';
 
-
-
 @Component({
   selector: 'app-client-home',
   standalone: true,
   imports: [RateStarsComponent, NewsComponent, FrameComponent],
   templateUrl: './client-home.component.html',
-  styleUrl: './client-home.component.scss'
+  styleUrls: ['./client-home.component.scss'],
 })
 export class ClientHomeComponent {
+  private endpoint = inject(ConnectionService);
 
-private endpoint = inject(ConnectionService);
+  publi: any[] = [];
+  publiDestaques: any[] = [];
+  currentIndex: number = 0;
 
-publi: any[] = [];
+  ngOnInit(): void {
+    this.LoadData();
+  }
 
-ngOnInit(): void {
-  this.LoadData();
-}
-
-LoadData() {
-  this.endpoint.getPublicacao().subscribe(
-    (resp: AnswerStandard) =>{
-      if(resp.success === 1) {
+  LoadData() {
+    this.endpoint.getPublicacao().subscribe((resp: AnswerStandard) => {
+      if (resp.success === 1) {
         this.publi = resp.data.publicacao;
-        console.log(this.publi);
+
+        // Filtrar somente as publicações de destaque
+        this.publiDestaques = this.publi.filter(
+          (item: any) => item.flagdestaque === 1
+        );
+
+        // Garantir que o índice inicial seja o primeiro destaque
+        this.currentIndex = 0;
       }
-    }
-  )
-}
+    });
+  }
 
-
+  goToSlide(index: number) {
+    this.currentIndex = index;
+  }
 }
