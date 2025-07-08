edfe46df679c79977006014750a2b9b4e996b961
Matheus Estevam <oliver.matheus@outlook.pt>
2025-07-08 17:25:44 -0300

    atualizado a logica do rateComponent

--- src/app/components/movie-rating/movie-rating.component.ts
+++ src/app/components/movie-rating/movie-rating.component.ts
@@ -8,19 +8,26 @@ import { Component, Input } from '@angular/core';
   styleUrl: './movie-rating.component.scss'
 })
 export class MovieRatingComponent {
-  @Input() overallScore: number = 8.5;
-  @Input() ratingCriteria = [
-    { name: 'Roteiro', score: 9 },
-    { name: 'Direção', score: 8 },
-    { name: 'Atuação', score: 8.5 },
-    { name: 'Cinematografia', score: 9.5 },
-    { name: 'Trilha Sonora', score: 7.5 }
-  ];
-
-  get filledStarCount(): number {
-    return Math.floor(this.overallScore / 2);
+  @Input() overallScore: number | null = null;
+  @Input() ratingCriteria : { name: string; score: number }[] = [];
+  
+  ngOnInit() {
+   if ((this.overallScore === null || this.overallScore === undefined) && this.ratingCriteria.length > 0) {
+      this.overallScore = this.calcOverAllScore();
+    }
   }
 
+  calcOverAllScore(): number {
+    const totalScore = this.ratingCriteria.reduce((sum, item) => sum + item.score, 0);
+    const avgScore = totalScore / this.ratingCriteria.length;
+    return avgScore;
+  }
+
+  get filledStarCount(): number {
+    const score = this.overallScore ?? 0;
+    return score / 2;
+  }
+  
   getStars() {
     return new Array(5);
   }
